a91c2c91815a03d57c73cf7a023387f3e7bacfa9
shakil islam <freelancershakil913@gmail.com>
2022-01-21 01:48:50 +0600

    methods writing

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var app = Vue.createApp({
+    
     data() {
-      return {
-        msg: "Hello World",
-        man: {
-            name: "shakil",
-            link: "https://freelancershakil913.blogspot.com",
-            htmlCode: "<a href='https://freelancershakil913.blogspot.com'>Linking here</a>"
+      return {}
+    },
+
+    methods: {
+        getCurrentTime(){
+            let ct = new Date();
+            return ct;
         }
-      };
     }
+
   });
   
   app.mount("#app");
