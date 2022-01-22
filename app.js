e37233940dc613e90aa5d1571a8ac0350d40efdc
shakil islam <freelancershakil913@gmail.com>
2022-01-23 05:21:14 +0600

    Style Binding

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data(){
         return{
-           activeClass : "red"
+           textColor : "red",
+           background: "yellow"
         }
     },
     methods:{
