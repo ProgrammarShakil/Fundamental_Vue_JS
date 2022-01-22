6eeb049816adfff3d07bddcf7cb5853704ffcf3b
shakil islam <freelancershakil913@gmail.com>
2022-01-22 19:25:17 +0600

    class binding

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
-
-
 const app = Vue.createApp({
     data(){
         return{
-            number:'',
-            result: ''
+           activeClass : "red"
         }
     },
     methods:{
-        
-        // handleInput(event){
-        //     console.log(event);
-        //     this.number = event.target.value;
-        // },               No Need to call - If i use v-model 
-
-        getDouble(){
-            this.result = this.number * 2
-        },
-        getSquare(){
-            this.result = this.number * this.number
-        },
-        reset(){
-            this.number = "",
-            this.result = ""
+        setClass(className){
+            this.activeClass = className
         }
     }
 })
