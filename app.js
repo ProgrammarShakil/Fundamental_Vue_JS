9f25375307a846efb3420c046038ac43a4b3122c
shakil islam <freelancershakil913@gmail.com>
2022-01-23 10:29:37 +0600

    Watch - String , Object / Nested property

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 const app = Vue.createApp({
     data(){
         return{
-          skills:[
-              {name:'html', experience: 5},
-              {name:'css', experience: 3},
-              {name:'js', experience: 6}
-            ],
-          newSkill: {} // we can also use sting here - as like  /-/ newSkill: "" /-/
+         mobile: "",
+         name:{
+             firstName: "",
+             lastName: ""
+         }
         }
     },
-    methods:{
-        addSkill(){
-            this.skills.push({name:this.newSkill, experience: 2 }); 
-            // we can also push sting here - as like  /-/ this.newSkill: "" /-/
-            this.newSkill = {}
-        },
-        removeSkill(item){
-            this.skills.splice(item, 1) 
-            // splice will remove index - here will remove current index 
-            // - take parameter as like item to use splice
-        }
-    }
+    watch:{
+        mobile(newValue, oldValue){
+            console.log(newValue);
+            if (isNaN(newValue)) {
+                alert('Enter Valid Mobile Number')
+                this.mobile = oldValue;  
+            }
+            if (this.mobile.length == 11) {
+                alert('You already Entered 11 Digit')
+                this.mobile = oldValue; 
+            }
+        },  // watch String value
+        'name.firstName': function(newValue, oldValue){
+            console.log(newValue);
+        }, // watch Object value - Nested Property
+
+        // 'name.lastName': function(newValue, oldValue){
+            // console.log(newValue);
+        // } // watch Object value -- Nested Property
+    } 
 })
 
 app.mount('#app');
