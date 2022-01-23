0186ca339921391fa29135b32ad134fbf3cc9c20
shakil islam <freelancershakil913@gmail.com>
2022-01-23 11:49:49 +0600

    methods vs computed vs watch

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 const app = Vue.createApp({
+    
     data(){
         return{
-         mobile: "",
          name:{
              firstName: "",
-             lastName: ""
+             lastName: "",
+             Fullname:''
          }
         }
     },
-    watch:{
-        mobile(newValue, oldValue){
-            console.log(newValue);
-            if (isNaN(newValue)) {
-                alert('Enter Valid Mobile Number')
-                this.mobile = oldValue;  
-            }
-            if (this.mobile.length == 11) {
-                alert('You already Entered 11 Digit')
-                this.mobile = oldValue; 
-            }
-        },  // watch String value
-        'name.firstName': function(newValue, oldValue){
-            console.log(newValue);
-        }, // watch Object value - Nested Property
 
-        // 'name.lastName': function(newValue, oldValue){
-            // console.log(newValue);
-        // } // watch Object value -- Nested Property
-    } 
+    watch:{
+        'name.firstName' : function(newValue){
+            this.name.Fullname =  newValue 
+        },   // target data property name object and take value - and for show take show var as like "Fullname" and show it view file - take it "name.Fullname" as like in view file
+        'name.lastName' : function(newValue){
+            this.name.Fullname =  newValue 
+        }   // target data property name object and take value - and for show take show var as like "Fullname" and show it view file - take it "name.Fullname" as like in view file
+    },
+
+    methods:{
+        // show(){
+        //     return this.name.firstName + " " + this.name.lastName;
+        // }   just make a function and return - and call it view file with ()
+    },
+
+    computed:{
+        // show(){
+        //     return this.name.firstName + " " + this.name.lastName;
+        // }    just make a function and return - and call it view file without ()
+    }
+
 })
 
 app.mount('#app');
