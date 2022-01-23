b620b11b98a41d564f769d4351c8254951ceb910
shakil islam <freelancershakil913@gmail.com>
2022-01-23 06:06:47 +0600

    list rendering - V-model - methods - v-for - event - push string - push object - splice

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const app = Vue.createApp({
     data(){
         return{
-           textColor : "red",
-           background: "yellow"
+          skills:[
+              {name:'html', experience: 5},
+              {name:'css', experience: 3},
+              {name:'js', experience: 6}
+            ],
+          newSkill: {} // we can also use sting here - as like  /-/ newSkill: "" /-/
         }
     },
     methods:{
-        setClass(className){
-            this.activeClass = className
+        addSkill(){
+            this.skills.push({name:this.newSkill, experience: 2 }); 
+            // we can also push sting here - as like  /-/ this.newSkill: "" /-/
+            this.newSkill = {}
+        },
+        removeSkill(item){
+            this.skills.splice(item, 1) 
+            // splice will remove index - here will remove current index 
+            // - take parameter as like item to use splice
         }
     }
 })
