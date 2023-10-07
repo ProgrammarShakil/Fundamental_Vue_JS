45a5d5a2a82642ed8d9e72e8b8a20300a7a5ae7f
programmarshakil <shakljoka103@gmail.com>
2023-10-07 23:45:14 +0600

    vue3 Methods

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,32 +1,27 @@
 <template>
   <div>
+    <button @click="callDemoPosts()">Call Demo Posts</button>
     <div>{{ demoPosts }}</div>
     <div>{{ $store.state.bookList }}</div>
   </div>
 </template>
 
 <script>
-import { onMounted, computed, ref } from "vue";
+import { ref } from "vue";
 export default {
   setup() {
-    const studentFirstName = ref("Shakil");
-    const studentLastName = ref("Islam");
 
     const demoPosts = ref([]);
 
-    const studentFullName = computed(() => {
-      return `${studentFirstName.value} ${studentLastName.value}`;
-    });
-
-    onMounted(() => {
+    function callDemoPosts(){
       fetch("https://jsonplaceholder.typicode.com/todos/1")
         .then((response) => response.json())
         .then((data) => {
           demoPosts.value = data
         });
-    });
+    }
 
-    return { demoPosts, studentFullName };
+    return { callDemoPosts, demoPosts };
   },
 };
 </script>
