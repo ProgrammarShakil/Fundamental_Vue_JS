ba9674ee2dd5984cadf5ad3a7013af3f27ddf129
programmarshakil <shakljoka103@gmail.com>
2023-10-07 21:53:20 +0600

    vue3 vuex4 config

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue' 
+import App from './App.vue' //root component
+import Store from './store/index'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+app.use(Store) //use store
+app.mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+
+import { createStore } from 'vuex'
+
+const store = createStore({
+    state:{
+        bookList: ['Book One']
+    }
+})
+
+export default store
