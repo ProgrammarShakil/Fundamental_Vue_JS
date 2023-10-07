87531f08009e7cdb4f5b233097cc7fda74ec7f19
programmarshakil <shakljoka103@gmail.com>
2023-10-07 17:07:46 +0600

    vuex install

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,26 +1,6 @@
 <template>
   <div>
-    <div>
-      <!-- data showing  -->
-      {{ name }}
-    </div>
-
-    <!-- v text  -->
-    <div v-text="vText"></div>
-
-    <!-- v-html  -->
-    <div v-html="Vhtml"></div>
-
-    <!-- dynamic class  -->
-    <div :class="color">testing</div>
-
-    <!-- dynamic string class  -->
-    <div :class="colorChange ? 'green' : 'red'">change color</div>
-
-    <!-- dynamic array class  -->
-    <div :class="[soldOut ? 'blue' : 'green']">IsSoldOut?</div>
-
-    <div :class="{ 'red': soldOut, 'green': !soldOut }">Avaliable</div>
+    <div>{{name}}</div>
   </div>
 </template>
 
@@ -28,25 +8,11 @@
 export default {
   data() {
     return {
-      name: "shakil",
-      vText: "vText",
-      Vhtml: "<b>hello, i am html from backend. </b>",
-      color: "red",
-      colorChange: true,
-      soldOut: true,
+      name: "shakil islam",
     };
   },
 };
 </script>
 
 <style>
-.green {
-  color: green;
-}
-.red {
-  color: red;
-}
-.blue {
-  color: blue;
-}
 </style>
