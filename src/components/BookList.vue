280aa83b57a18917a492cb631fd0033d7ff668b7
ProgrammarShakil <shakljoka103@gmail.com>
2023-10-19 03:29:58 +0600

    vue3 Reactive

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,27 +1,37 @@
 <template>
   <div>
-    <button @click="callDemoPosts()">Call Demo Posts</button>
+    <!-- <button @click="callDemoPosts()">Call Demo Posts</button>
     <div>{{ demoPosts }}</div>
-    <div>{{ $store.state.bookList }}</div>
+    <div>{{ $store.state.bookList }}</div> -->
+    <div>{{ state.firstName }}</div>
+    <div>{{ state.lastName }}</div>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { reactive, computed } from "vue";
 export default {
   setup() {
 
-    const demoPosts = ref([]);
+    const state=reactive({
+      firstName: 'Shakil',
+      lastName: 'islam',
+      fullName:computed(()=>{
+        return `${state.firstName} ${state.lastName}`
+      })
+    })
 
-    function callDemoPosts(){
-      fetch("https://jsonplaceholder.typicode.com/todos/1")
-        .then((response) => response.json())
-        .then((data) => {
-          demoPosts.value = data
-        });
-    }
+    // const demoPosts = ref([]);
 
-    return { callDemoPosts, demoPosts };
+    // function callDemoPosts(){
+    //   fetch("https://jsonplaceholder.typicode.com/todos/1")
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //       demoPosts.value = data
+    //     });
+    // }
+
+    return { state };
   },
 };
 </script>
